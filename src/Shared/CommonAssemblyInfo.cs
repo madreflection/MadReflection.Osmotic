a130eb8fe7d089a98c9a16fa2c193440fee01051
Benn Benson <madreflection@live.com>
2019-01-15 21:09:15 -0800

    Updated the copyright.  Minor version bump.

--- src/Shared/CommonAssemblyInfo.cs
+++ src/Shared/CommonAssemblyInfo.cs
@@ -3,11 +3,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MadReflection.Osmotic")]
 [assembly: AssemblyProduct("Osmotic")]
-[assembly: AssemblyCopyright("Copyright © 2017-2018 Benn Benson")]
+[assembly: AssemblyCopyright("Copyright © 2017-2019 Benn Benson")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
